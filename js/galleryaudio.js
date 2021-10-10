f89d60dcc012a1b67af0fac73edb268344ecee41
sulochanawp <89105912+sulochanawp@users.noreply.github.com>
2021-10-10 09:02:02 +0530

    Add files via upload

---
+++ js/galleryaudio.js
@@ -0,0 +1,68 @@
+var x1 = document.getElementById("myAudio1");
+
+function playAudio1() {
+  x1.play();
+}
+
+function pauseAudio1() {
+  x1.pause();
+}
+
+
+
+var x2 = document.getElementById("myAudio2");
+
+function playAudio2() {
+  x2.play();
+}
+
+function pauseAudio2() {
+  x2.pause();
+}
+
+
+
+var x3 = document.getElementById("myAudio3");
+
+function playAudio3() {
+  x3.play();
+}
+
+function pauseAudio3() {
+  x3.pause();
+}
+
+
+
+var x4 = document.getElementById("myAudio4");
+
+function playAudio4() {
+  x4.play();
+}
+
+function pauseAudio4() {
+  x4.pause();
+}
+
+
+
+var x5 = document.getElementById("myAudio5");
+
+function playAudio5() {
+  x5.play();
+}
+
+function pauseAudio5() {
+  x5.pause();
+}
+
+
+var x6 = document.getElementById("myAudio6");
+
+function playAudio6() {
+  x6.play();
+}
+
+function pauseAudio6() {
+  x6.pause();
+}
